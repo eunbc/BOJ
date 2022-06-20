d5b82cb3de5300f235622d6fb6f97b3a1776b26d
Eunbi <joeunbi0514@gmail.com>
2022-06-20 19:38:47 +0900

    2490 my solution

---
+++ src/Main_2490.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main_2490 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] array = new int[12];
+        for(int i=0; i<12; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        play(Arrays.copyOfRange(array,0,4));
+        play(Arrays.copyOfRange(array,4,8));
+        play(Arrays.copyOfRange(array,8,12));
+    }
+
+    private static void play(int[] arr) {
+        int k = 0;
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] == 1) k++;
+        }
+
+        switch (k) {
+            case 3:
+                System.out.println("A");
+                break;
+            case 2:
+                System.out.println("B");
+                break;
+            case 1:
+                System.out.println("C");
+                break;
+            case 0:
+                System.out.println("D");
+                break;
+            case 4:
+                System.out.println("E");
+                break;
+        }
+    }
+
+}
