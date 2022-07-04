ddd7ee2746b4f6ca5e897afd5762db94bf5f6f55
Eunbi <joeunbi514@naver.com>
2022-07-04 20:44:14 +0900

    first use of bufferedReader

---
+++ src/Main_15552_2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main_15552_2 {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0; i<t; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+        }
+    }
+
+}
