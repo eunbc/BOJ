719302528eba7d99f8e231aec458641f54ff4b55
Eunbi <joeunbi514@naver.com>
2022-07-05 21:30:16 +0900

    draw stars...

---
+++ src/Main_2439.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main_2439 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=1; i<n+1; i++) {
+            for(int k=n; k>i; k--) {
+                System.out.print(" ");
+            }
+            for(int j=1; j<i+1; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+
+}
